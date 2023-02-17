eff829ca1b0a6601413faaff6d43363fc338561a
Mahenge <k.mahenge@gmail.com>
2023-02-17 10:43:53 +0300

    Debug: exception errors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,15 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import { AllExceptionsFilter } from './utils/all-exceptions.filter';
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   app
     .listen(process.env.PORT)
     .then(() => {
       console.log('successfully stared on port ' + process.env.PORT);
     })
     .catch((error) => {
+      console.log('bootstrap error');
       console.log(error);
     });
 }
